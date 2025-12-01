eb95ebb105864a658b0d55d626f06f52b96e0dab
Andino Faturahman <andino@ptgdi.com>
2025-12-01 21:18:01 +0700

    hide history for user

--- src/components/PricingCatalogueCard.vue
+++ src/components/PricingCatalogueCard.vue
@@ -1,10 +1,17 @@
 <template>
-  <DocumentCard :data="props.template" variant="pricing" @download="handleDownload" @show-history="handleShowHistory" />
+  <DocumentCard
+    :data="props.template"
+    :hide-history-button="authStore.isUser"
+    variant="pricing"
+    @download="handleDownload"
+    @show-history="handleShowHistory" />
 </template>
 
 <script setup lang="ts">
 import type { CatalogueItem } from "@/data/mockData";
 import DocumentCard from "./DocumentCard.vue";
+import { useAuthStore } from "@/stores/auth";
+const authStore = useAuthStore();
 
 const props = defineProps<{
   template: CatalogueItem;
